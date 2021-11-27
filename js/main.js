d0242ad7fe9502e438ecaf8a9a746f22e6811b59
onset1 <learningtoomuch@protonmail.com>
2021-11-27 07:54:28 -0300

    Log in

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
+// JS functions for Sign up form
 document.getElementById("sign-up-a").addEventListener("click",function () {
     document.querySelector(".pop-up-container").style.display = "flex";
 });
-
+document.getElementById("log-in-c").addEventListener("click",function () {
+    document.querySelector(".pop-up-container").style.display = "none";
+    document.querySelector(".pop-up-container-log-in").style.display = "flex";
+});
 document.querySelector(".pop-up-close").addEventListener("click", function () {
     document.querySelector(".pop-up-container").style.display = "none";
-})
+});
+// JS functions for Log in form
+document.getElementById("log-in-a").addEventListener("click",function () {
+    document.querySelector(".pop-up-container-log-in").style.display = "flex";
+});
+document.getElementById("sign-up-c").addEventListener("click",function () {
+    document.querySelector(".pop-up-container-log-in").style.display = "none";
+    document.querySelector(".pop-up-container").style.display = "flex";
+});
+document.querySelector(".pop-up-close-log-in").addEventListener("click", function () {
+    document.querySelector(".pop-up-container-log-in").style.display = "none";
+});
+
