09a586b40d3bb1f277e207c480d17ce438858e37
onset1 <learningtoomuch@protonmail.com>
2021-11-28 10:52:41 -0300

    Submit post

--- js/main.js
+++ js/main.js
@@ -20,4 +20,21 @@ document.getElementById("sign-up-c").addEventListener("click",function () {
 document.querySelector(".pop-up-close-log-in").addEventListener("click", function () {
     document.querySelector(".pop-up-container-log-in").style.display = "none";
 });
+// JS functions for submitting a post
+document.getElementById("new-post").addEventListener("click",function () {
+    document.getElementById("post-area").style.display = "flex";
+});
+document.getElementById("reset-and-cancel").addEventListener("click",function () {
+    document.getElementById("post-area").style.display = "none";
+});
+var post= document.getElementById("submit-post");
+post.addEventListener("click", function(){
+    var commentBoxValue= document.getElementById("post-text").value;
+ 
+    var p = document.createElement("p");
+    var text = document.createTextNode(commentBoxValue);
+    p.appendChild(text);
+    document.getElementById("new-article").appendChild(p);
+ 
+});
 
